15b623b7886661fa763c5c78b8ddaec0b0208837
Christian Acevedo <christian.acevedo14@upr.edu>
2022-12-04 22:16:55 -0400

    Fixed small search bug

--- client/pages/search.js
+++ client/pages/search.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react'
 import { MediaCard } from "../ClientComponents/MediaCard"
 import ToggleButton from '@mui/material/ToggleButton';
 import ToggleButtonGroup from '@mui/material/ToggleButtonGroup';
+import { Table, TableCell } from '@mui/material';
 
 export default function Search() {
 
@@ -10,9 +11,13 @@ export default function Search() {
     const [results, setResults] = useState([]);
     const [buttonClicked, setbuttonClicked] = useState(true);
     const [alignment, setAlignment] = React.useState('web');
+    const [rows,setRows] = useState([]);
 
     const handleFalse = () => setbuttonClicked((buttonClicked) => false);
     const handleTrue = () => setbuttonClicked((buttonClicked) => true);
+
+
+
 
     const handleChange = (event, newAlignment) => {
         setAlignment(newAlignment);
@@ -24,17 +29,24 @@ export default function Search() {
 
         setQuery(e.target.value);
         // Fetch link not making use of env.local
-        if (buttonClicked === true) {
+        if (buttonClicked) {
             fetch(`https://api.themoviedb.org/3/search/movie?api_key=468018e64d6cfa119009ede09787dea0&language=en-US&page=1&include_adult=false&query=${e.target.value}`
             )
                 .then((res) => res.json())
                 .then((data) => {
-                    console.log(data);
+                    console.log(data.results);
                     if (!data.errors) {
                         setResults(data.results);
+                        let rows = data.results.reduce(function (rows, key, index) {
+                            return (index % 4 == 0 ? rows.push([key])
+                                : rows[rows.length - 1].push(key)) && rows;
+                        }, []);
+                        setRows(rows);
                     } else {
                         setResults([]);
+                        setRows([])
                     }
+
                 });
 
         }
@@ -44,11 +56,17 @@ export default function Search() {
             )
                 .then((res) => res.json())
                 .then((data) => {
-                    console.log(data);
+                    console.log(data.results);
                     if (!data.errors) {
                         setResults(data.results);
+                        let rows = results.reduce(function (rows, key, index) {
+                            return (index % 2 == 0 ? rows.push([key])
+                                : rows[rows.length - 1].push(key)) && rows;
+                        }, []);
+                        setRows(rows);
                     } else {
                         setResults([]);
+                        setRows([]);
                     }
                 });
         }
@@ -78,10 +96,14 @@ export default function Search() {
 
                 {results.length > 0 && (
                     <ul className="results">
-                        {results.map(media => (
-                            <li key={media.id}>
-                                <MediaCard media={media} type={buttonClicked} />
-                            </li>
+                        {rows.map(row => (
+                            <Table>
+                                {row.map(media => (
+                                    <TableCell key={media.id}>
+                                        <MediaCard media={media} type={buttonClicked} />
+                                    </TableCell>
+                                ))}
+                            </Table>
                         ))}
                     </ul>
                 )}
