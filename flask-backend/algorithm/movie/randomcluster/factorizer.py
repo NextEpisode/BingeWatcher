1f9e1ae99339ad4be4b91c57900c3f4cafa40700
ErickRodz <erick.rodriguez15@upr.edu>
2022-12-06 14:47:57 -0400

    Algorithm merged into main

---
+++ flask-backend/algorithm/movie/randomcluster/factorizer.py
@@ -0,0 +1,20 @@
+import torch
+import numpy as np
+
+class MatrixFactorization(torch.nn.Module):
+    def __init__(self, n_users, n_items, n_factors=20):
+        super().__init__()
+        #These are the equivalent of lookup tables in a matrix of sorts.
+        self.user_factors = torch.nn.Embedding(n_users, n_factors)
+        self.item_factors = torch.nn.Embedding(n_items, n_factors)
+        #Weight factor assignation is to specify the importance of a datapoint in the matrix.
+        self.user_factors.weight.data.uniform_(0, 0.05)
+        self.item_factors.weight.data.uniform_(0, 0.05)
+        
+    def forward(self, data):
+        # matrix multiplication
+        users, items = data[:,0], data[:,1]
+        return (self.user_factors(users)*self.item_factors(items)).sum(1)
+    
+    def predict(self, user, item):
+        return self.forward(user, item)
---
+++ flask-backend/algorithm/movie/randomcluster/loader.py
@@ -0,0 +1,39 @@
+from torch.utils.data.dataset import Dataset
+import pandas as pd
+import torch
+
+# Note: This isn't 'good' practice, in a MLops sense but we'll roll with this since the data is already loaded in memory.
+class Loader(Dataset):
+
+    def __init__(self):
+        ratings_df = pd.read_csv(r'flask-backend\algorithm\dataset\ml-latest-small\csv_0.csv')
+        #ratings_df = pd.read_csv(r'flask-backend\algorithm\dataset\ml-latest-small\ratings.csv')
+        self.ratings = ratings_df.copy()
+        
+        # Extract all user IDs and movie IDs
+        users = ratings_df.userId.unique()
+        movies = ratings_df.movieId.unique()
+        
+        #--- Producing new continuous IDs for users and movies ---
+        
+        # Unique values : index
+        self.userid2idx = {o:i for i,o in enumerate(users)}
+        self.movieid2idx = {o:i for i,o in enumerate(movies)}
+        
+        # Obtained continuous ID for users and movies
+        self.idx2userid = {i:o for o,i in self.userid2idx.items()}
+        self.idx2movieid = {i:o for o,i in self.movieid2idx.items()}
+        
+        # return the id from the indexed values as noted in the lambda function down below.
+        self.ratings.movieId = ratings_df.movieId.apply(lambda x: self.movieid2idx[x])
+        self.ratings.userId = ratings_df.userId.apply(lambda x: self.userid2idx[x])
+        
+        self.x = self.ratings.drop(['rating', 'timestamp'], axis=1).values
+        self.y = self.ratings['rating'].values
+        self.x, self.y = torch.tensor(self.x), torch.tensor(self.y) # Transforms the data to tensors (ready for torch models.)
+
+    def __getitem__(self, index):
+        return (self.x[index], self.y[index])
+
+    def __len__(self):
+        return len(self.ratings)
